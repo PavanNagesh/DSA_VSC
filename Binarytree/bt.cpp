ca8e9a5abf413c33d729800f9ae508fe827df019
PavanNagesh <pavannagesh681@gmail.com>
2026-08-15 23:42:11 +0530

    bt

---
+++ Binarytree/bt.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+Node* buildTree(vector<int> preOrder){
+    idx++;
+
+    if(preOrder[idx] == -1){
+        return NULL;
+    }
+
+    Node* root = new Node (preOrder[idx]);
+    root -> left = buildTree(preOrder);
+    root -> right = buildTree(preOrder);
+
+    return root;
+}
+
+void preOrder(Node* root){
+    if(root == NULL){
+        return;
+    }
+    cout << root -> data;
+    preOrder(root -> left);
+    preOrder(root -> right);
+}
+
+void inOrder(Node* root){
+    if(root == NULL){
+        return;
+    }
+
+    inOrder(root -> left);
+    cout << root -> data;
+    inOrder(root -> right);
+}
+
+void postOrder(Node* root){
+    if(root == NULL){
+        return;
+    }
+
+    postOrder(root -> left);
+    postOrder(root -> right);
+    cout << root -> data;
+}
+
+int main(){
+    vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+
+    Node* root = buildTree(preorder);
+
+    cout << "PreOrder: "; 
+    preOrder(root);
+    cout << endl;
+
+    cout << "InOrder: "; 
+    inOrder(root);
+    cout << endl;
+
+    cout << "PostOrder: "; 
+    postOrder(root);
+    cout << endl;
+}
